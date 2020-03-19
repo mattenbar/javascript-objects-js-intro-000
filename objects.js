db3971e33a41846a72662174caa1d9a661dd2e43
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-19 16:30:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,6 @@
 var playlist = {artist : 'songTitle'}
 
 function updatePlaylist(playlist , artistName, songTitle){
-  playlist = {[artistName] : 'songTitle'}
+  playlist[artistName] = "songTitle"
   return playlist
 }
-
